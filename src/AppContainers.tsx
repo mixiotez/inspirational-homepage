89cc8f4352970fff7768c67a4147e56f4d9b99ca
Miguel Téllez <miguel.tellez1997@gmail.com>
2024-05-24 11:49:31 -0600

    Update Cards and Weather + Quote

--- src/AppContainers.tsx
+++ src/AppContainers.tsx
@@ -4,13 +4,32 @@ export const CardsContainer = styled.div`
   width: calc(100% - 64px);
   display: flex;
   gap: 32px;
+
+  @media screen and (max-width: 1023px) {
+    width: calc(100% - 48px);
+    gap: 16px;
+  }
+
+  @media screen and (max-width: 767px) {
+    width: calc(100% - 32px);
+    flex-direction: column;
+    gap: 8px;
+  }
 `;
 
 export const AppContainer = styled(CardsContainer)`
   overflow: hidden;
   height: calc(100vh - 32px);
   margin: 16px 32px;
-  gap: 32px;
   flex-direction: column;
   align-items: center;
+
+  @media screen and (max-width: 1023px) {
+    margin: 16px 24px;
+  }
+
+  @media screen and (max-width: 767px) {
+    height: calc(100vh - 16px);
+    margin: 8px 16px;
+  }
 `;
--- src/components/Weather/WeatherContainer.tsx
+++ src/components/Weather/WeatherContainer.tsx
@@ -2,20 +2,19 @@ import styled from 'styled-components';
 import Card from '../common/Card';
 
 export const WeatherContainer = styled(Card)`
-  flex-basis: 20%;
-
   img {
-    margin: -26px; // The png is badly cropped. This helps align it better.
+    margin: -25px; // The png is badly cropped. This helps align it better.
   }
 
   div {
-    flex-basis: 40%;
     display: flex;
     flex-direction: column;
     align-items: flex-start;
     justify-content: space-evenly;
 
     &:first-child {
+      flex-grow: 1;
+      gap: 4px;
       min-width: 72px;
       border-right: 2px dotted rgba(0, 0, 0, 0.5);
 
@@ -27,10 +26,46 @@ export const WeatherContainer = styled(Card)`
 
     &:last-child {
       flex-basis: 60%;
+    }
+
+    b {
+      font-weight: 500;
+      font-size: 1.5rem;
+    }
+  }
+
+  @media screen and (max-width: 1023px) {
+    img {
+      margin: -20px;
+      width: 80px;
+      height: 80px;
+    }
+  }
+
+  @media screen and (max-width: 767px) {
+    img {
+      margin: -18px;
+      width: 72px;
+      height: 72px;
+    }
+
+    div {
+      align-items: center;
+
+      &:first-child {
+        flex-basis: 50%;
+      }
+
+      &:last-child {
+        flex-basis: 50%;
+      }
+
+      p {
+        font-size: 0.75rem;
+      }
 
       b {
-        font-weight: 500;
-        font-size: 1.5rem;
+        font-size: 1.25rem;
       }
     }
   }
